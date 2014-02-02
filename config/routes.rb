7136036805bdf42c46dfd5844683d7168cc8f2b0
Glenn Cameron <glenn@eggify.com>
2014-02-02 11:46:53 -0800

    added a contact page and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
