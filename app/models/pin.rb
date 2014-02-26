d30766e1e8e685c37fde5e7981ffb40a6176e44d
Glenn Cameron <glenn@eggify.com>
2014-02-25 22:55:39 -0800

    Added names to users and such

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,6 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }
+     validates :image, presence: true
+	 validates :description, presence: true
 end
